eb8a033d212ccdbfaeb808a690babd18edfbfe33
Samit Kumar Sinha <samitkumarsinha@gmail.com>
2021-06-06 10:31:19 +0530

    Update index.js
    difference in use of router

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var api = require("./routes/api");
 var express = require("express");
 var app = express();
-app.use("/api", api);
+app.use("/api", api); //path localhost:3000/api/someapi
+// app.use(api); //path localhost:3000/someapi
 var cors = require('cors')
 app.use(cors());
 app.listen(3000, () => {
